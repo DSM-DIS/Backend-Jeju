ef44bc5988ac9cdf1fe4ffa67637883f2b00588d
MinJun Seo <smj0385@naver.com>
2020-08-30 15:20:31 +0900

    Use dotenv module to make more safe code
    데이터베이스와 관련된 정보들은 .env 파일에 담아 따로 관리하고, 실제 데이터베이스의 정보에 접근할 때는 다음 키를 통해서 접근합니다.

---
+++ configs/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DBNAME: process.env.DB_DBNAME
+};
