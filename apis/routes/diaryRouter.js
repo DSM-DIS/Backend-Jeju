abf62309f5622f4c5062b4649cf47fa101b63c1c
MinJun Seo <smj0385@naver.com>
2020-09-05 14:21:31 +0900

    Add execption handling with using next
    next()를 통한 예외 처리 추가

--- apis/routes/diaryRouter.js
+++ apis/routes/diaryRouter.js
@@ -6,32 +6,41 @@ const { Diary } = require('../../models');
 const diaryService = new DiaryService(Diary);
 
 // 전반적으로 인증 middleware 필요
-router.get('/:id/writing', async (req, res) => {
+router.get('/:id/writing', async (req, res, next) => {
   const diaryBook = parseInt(req.params.id);
   const { author } = qs.parse(req.query);
 
-  const id = await diaryService.writingDiary(diaryBook, author);
-
-  res.status(200).json({ id: id });
+  try {
+    const id = await diaryService.writingDiary(diaryBook, author);
+    res.status(200).json({ id: id });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/:id/hand', async (req, res) => {
+router.post('/:id/hand', async (req, res, next) => {
   const diaryBook = parseInt(req.params.id);
   const { id, content } = req.body;
 
-  const page = await diaryService.handingDiary(diaryBook, id, content);
-
-  // owner 변경을 하는 작업 필요
-  res.status(302).redirect(`/diary/${diaryBook}/reading?page=${page}`);
+  try {
+    const page = await diaryService.handingDiary(diaryBook, id, content);
+    // owner 변경을 하는 작업 필요
+    res.status(302).redirect(`/diary/${diaryBook}/reading?page=${page}`);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id/reading', async (req, res) => {
+router.get('/:id/reading', async (req, res, next) => {
   const diaryBook = parseInt(req.params.id);
   const page = (req.query.page) ? parseInt(qs.parse(req.query).page) : 1;
 
-  const content = await diaryService.readingDiary(diaryBook, page);
-
-  res.status(200).json({ page: page, content: content });
+  try {
+    const content = await diaryService.readingDiary(diaryBook, page);
+    res.status(200).json({ page: page, content: content });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
