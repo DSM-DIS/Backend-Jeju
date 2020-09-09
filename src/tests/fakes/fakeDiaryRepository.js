39a5419eed41d1462acdcaeeb969c099aa860cb4
MinJunSeo <smj0385@naver.com>
2020-09-10 07:40:22 +0900

    Add fake repostiroy for testing

---
+++ src/tests/fakes/fakeDiaryRepository.js
@@ -0,0 +1,20 @@
+const { notFoundDiary, notFoundPage } = require('../../errors');
+
+class FakeDiaryRepository {
+  static async readingDiary(diaryBook, page = 1) {
+    if (diaryBook !== 1) {
+      throw notFoundDiary;
+    }
+    if (page !== 1) {
+      throw notFoundPage;
+    }
+    if (diaryBook === 1 && page === 1) {
+      return {
+        page: 1,
+        content: 'test1'
+      };
+    }
+  }
+}
+
+module.exports = FakeDiaryRepository;
