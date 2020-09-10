74d3535860d18c19f722ea7353ca56d26f47674e
MinJunSeo <smj0385@naver.com>
2020-09-10 10:59:33 +0900

    Add internal server error

--- src/errors/index.js
+++ src/errors/index.js
@@ -9,9 +9,11 @@ class HttpError extends Error {
 const badRequest = new HttpError(400, '잘못된 방식의 요청입니다.');
 const notFoundDiary = new HttpError(404, '해당 일기를 찾을 수 없습니다.', 'diary');
 const notFoundPage = new HttpError(404, '해당 page를 찾을 수 없습니다.', 'page');
+const internalServer = new HttpError(500, '내부적으로 오류가 있습니다. 빠른 수정 바랍니다.');
 
 module.exports = {
   badRequest,
   notFoundDiary,
-  notFoundPage
+  notFoundPage,
+  internalServer
 };
