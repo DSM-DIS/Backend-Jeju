6f95b92371c94218d2d204bfe8f3c2a86b0c77d6
MinJunSeo <smj0385@naver.com>
2020-09-10 11:21:56 +0900

    Fix error handler

--- src/tests/fakes/fakePages.js
+++ src/tests/fakes/fakePages.js
@@ -10,11 +10,6 @@ class FakeDiaryPages {
       }
 
       const lastPage = await getLastPage(Diary, diaryBook);
-      
-      // 해당 diaryBook이 존재하지 않을 때는 0이 반환된다.
-      if (!lastPage) {
-        throw notFoundDiary;
-      }
       return lastPage;
     } catch (error) {
       throw error;
