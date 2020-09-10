fcdd763a0eff9a8fbf6d5f77d85bb9cac8b635bd
MinJunSeo <smj0385@naver.com>
2020-09-10 18:54:06 +0900

    Remove some errors because don't use

--- src/errors/index.js
+++ src/errors/index.js
@@ -9,13 +9,9 @@ class HttpError extends Error {
 const badRequest = new HttpError(400, '잘못된 방식의 요청입니다.');
 const notFoundDiary = new HttpError(404, '해당 일기를 찾을 수 없습니다.', 'diary');
 const notFoundPage = new HttpError(404, '해당 page를 찾을 수 없습니다.', 'page');
-const invalidPage = new HttpError(412, 'page의 형식이 올바르지 않습니다.', 'page');
-const internalServer = new HttpError(500, '내부적으로 오류가 있습니다. 빠른 수정 바랍니다.');
 
 module.exports = {
   badRequest,
   notFoundDiary,
-  notFoundPage,
-  invalidPage,
-  internalServer
+  notFoundPage
 };
