8459cc7a604fb4f2a9dce1114cca546a58e4e54d
MinJunSeo <smj0385@naver.com>
2020-09-11 16:33:42 +0900

    Add getLastId util test code

---
+++ src/tests/getLastId.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+const { getLastId } = require('../utils');
+const { Diary } = require('../module');
+const { notFoundDiary } = require('../errors');
+
+describe('# print last id', () => {
+  it('console.log()', async () => {
+    try {
+      const id = await getLastId(Diary, 1);
+      console.log(id);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  it('error', async () => {
+    await expect(getLastId(Diary, 2))
+      .to.be.rejectedWith(notFoundDiary);
+  });
+});
