301cac03638f8bc11c330562b8bc6e09360deda0
MinJunSeo <smj0385@naver.com>
2020-09-11 16:31:23 +0900

    Use sequelize.max method

--- src/utils/getLastId.js
+++ src/utils/getLastId.js
@@ -1,9 +1,13 @@
 // 해당 diaryBook에서 가장 마지막으로 생성된 튜플의 id를 반환
 
+const { notFoundDiary } = require('../errors');
+
 module.exports = async (diaryModel, diaryBook) => {
-  return await diaryModel.findOne({
-    attributes: ['id'],
-    where: { diary_book_id: diaryBook } ,
-    order: [['id', 'DESC']]
+  const lastId =  await diaryModel.max('id', {
+    where: { diary_book_id: diaryBook }
   });
+  if (isNaN(lastId)) {
+    throw notFoundDiary;
+  }
+  return lastId;
 };
