fb1a405120f4f4a110eed2ccc89552829c855b2c
MinJunSeo <smj0385@naver.com>
2020-10-25 16:11:11 +0900

    feat: invalid token error

--- src/errors/index.js
+++ src/errors/index.js
@@ -6,9 +6,11 @@ class HttpError extends Error {
 }
 
 const BAD_REQUEST = new HttpError(400, 'Invalid parameters supplied');
+const INVALID_TOKEN = new HttpError(403, 'Invalid Token');
 const NOT_FOUND = new HttpError(404, 'Not found');
 
 module.exports = {
   BAD_REQUEST,
+  INVALID_TOKEN,
   NOT_FOUND
 };
