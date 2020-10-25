0f41f908d9577be150a3281a2c1ec1befbd09fd2
MinJunSeo <smj0385@naver.com>
2020-10-25 16:41:58 +0900

    feat: writingDiary service method

--- src/services/diary.js
+++ src/services/diary.js
@@ -13,7 +13,7 @@ class DiaryService {
     if (typeof page !== 'number' || page < 1) {
       throw BAD_REQUEST;
     }
-    
+
     const res = await axios.get(`/repositories/diary-book/${diaryBookId}?page=${page}`);
 
     if (res.status === 400) {
@@ -26,6 +26,27 @@ class DiaryService {
       page: res.page,
       content: res.content
     };
+  }
+
+  writingDiary = async (userId, diaryBookId, content) => {
+    const CONTENT_MAX_LEN = 240;
+
+    if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
+      throw BAD_REQUEST;
+    }
+    if (typeof content !== 'string' || !content || content.length > CONTENT_MAX_LEN) {
+      throw BAD_REQUEST;
+    }
+
+    const res = await axios.post('/repositories/diary', {
+      userId,
+      diaryBookId,
+      content
+    });
+
+    if (res.status === 400) {
+      throw BAD_REQUEST;
+    }
   }
 }
 
