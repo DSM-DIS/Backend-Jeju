7407bd1501c50a3d43dec41e3c0747b83cdba371
MinJunSeo <smj0385@naver.com>
2020-10-25 16:49:26 +0900

    Fix: routing

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const apis = require('../apis');
+const router = require('../routes');
 
 module.exports = (app) => {
   app.use(cors); // CORS 허용
@@ -9,5 +9,5 @@ module.exports = (app) => {
   // 내부적으로 qs module을 사용하도록 설정
   app.use(express.urlencoded({ extended: true }));
 
-  app.use(apis);
+  app.use('/', router);
 };
