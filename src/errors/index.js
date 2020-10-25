bbde1478df318973219c3623d3b1dbc1b4da54e5
MinJunSeo <smj0385@naver.com>
2020-10-25 15:25:07 +0900

    Fix: fix error message

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,17 +1,14 @@
 class HttpError extends Error {
-  constructor(status, message, cause) {
+  constructor(status, message) {
     super(message);
     this.status = status;
-    this.cause;
   }
 }
 
-const badRequest = new HttpError(400, '잘못된 방식의 요청입니다.');
-const notFoundDiary = new HttpError(404, '해당 일기를 찾을 수 없습니다.', 'diary');
-const notFoundPage = new HttpError(404, '해당 page를 찾을 수 없습니다.', 'page');
+const BAD_REQUEST = new HttpError(400, 'Invalid parameters supplied');
+const NOT_FOUND = new HttpError(404, 'Not found');
 
 module.exports = {
-  badRequest,
-  notFoundDiary,
-  notFoundPage
+  BAD_REQUEST,
+  NOT_FOUND
 };
