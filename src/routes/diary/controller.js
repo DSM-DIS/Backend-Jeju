5f702eb0648f52f51de35f93d65fb2730a7ed8d6
MinJunSeo <smj0385@naver.com>
2020-10-25 16:43:20 +0900

    feat: writingDiary controller

--- src/routes/diary/controller.js
+++ src/routes/diary/controller.js
@@ -25,4 +25,32 @@ const getDiary = async (req, res) => {
       message: error
     });
   }
-}
+};
+
+const writingDiary = async (req, res) => {
+  try {
+    if (!Object.keys(req.body).length) {
+      throw BAD_REQUEST;
+    }
+
+    const userId = req.headers.Authorization;
+    const diaryBookId = parseInt(req.params.id);
+    const { content } = req.body;
+
+    if (!isAlrightId(userId) || isNaN(diaryBookId)) {
+      throw BAD_REQUEST;
+    }
+
+    await diaryService.writingDiary(userId, diaryBookId, content);
+    res.status(201).send();
+  } catch (error) {
+    res.status(error.status).send({
+      message: error.message
+    });
+  }
+};
+
+module.exports = {
+  getDiary,
+  writingDiary
+};
