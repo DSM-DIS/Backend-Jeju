1de201af4cefb97b1373294ddeec535c1caf2180
MinJunSeo <smj0385@naver.com>
2020-10-25 16:21:20 +0900

    fix: 403 http error

--- src/errors/index.js
+++ src/errors/index.js
@@ -6,11 +6,11 @@ class HttpError extends Error {
 }
 
 const BAD_REQUEST = new HttpError(400, 'Invalid parameters supplied');
-const INVALID_TOKEN = new HttpError(403, 'Invalid Token');
+const FORBIDDEN = new HttpError(403, 'Forbidden user access');
 const NOT_FOUND = new HttpError(404, 'Not found');
 
 module.exports = {
   BAD_REQUEST,
-  INVALID_TOKEN,
+  FORBIDDEN,
   NOT_FOUND
 };
