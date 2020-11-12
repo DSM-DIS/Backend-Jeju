d025573ac40871a6ae282ee8cc3300a28781fddc
MinJunSeo <smj0385@naver.com>
2020-11-12 19:18:30 +0900

    refactor: using server port env var

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const { SERVER_PORT } = require('./configs');
 
 const initApp = require('./loaders');
 
 initApp(app);
 
-app.set('port', process.env.SERVER_PORT || 4008);
-app.listen(app.get('port'), () => {
+app.listen(SERVER_PORT, () => {
   console.log('listing at ', app.get('port'));
 });
