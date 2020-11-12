09d89db2554e965a343acefd752e2ce2aa3154a7
MinJunSeo <smj0385@naver.com>
2020-11-12 19:18:42 +0900

    pref: set server port env var

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+  SERVER_PORT: process.env.SERVER_PORT,
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
