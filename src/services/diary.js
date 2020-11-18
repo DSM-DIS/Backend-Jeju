9bbafbdc917f9a659aea8e2333a6769b9842ab5e
MinJunSeo <smj0385@naver.com>
2020-11-18 13:31:25 +0900

    feat: make construcotr function for diary service

--- src/services/diary.js
+++ src/services/diary.js
@@ -1,16 +1,13 @@
 const axios = require('axios').default;
-const { BAD_REQUEST, NOT_FOUND } = require('../errors');
+const { Diary } = require('../repositories');
+const { isIntegerArg } = require('../utils');
+const { BAD_REQUEST, NOT_FOUND_DIARY_BOOK, NOT_FOUND_PAGE } = require('../errors');
 
-class DiaryService {
-  constructor(diaryModel) {
-    this.diaryModel = diaryModel;
-  }
-
-  getDiary = async (diaryBookId, page) => {
-    if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
-      throw BAD_REQUEST;
-    }
-    if (typeof page !== 'number' || page < 1) {
+function DiaryService() {
+  this.model = Diary;
+  
+  this.getDiary = async (diaryBookId, page) => {
+    if (!isIntegerArg(diaryBookId) || !isIntegerArg(page)) {
       throw BAD_REQUEST;
     }
 
@@ -19,35 +16,62 @@ class DiaryService {
     if (res.status === 400) {
       throw BAD_REQUEST;
     } else if (res.status === 404) {
-      throw NOT_FOUND;
+      if (res.cause === 'diary book') {
+        throw NOT_FOUND_DIARY_BOOK;
+      } else if (res.cause === 'page') {
+        throw NOT_FOUND_PAGE;
+      }
     }
-
-    return {
-      page: res.page,
-      content: res.content
-    };
+    return res;
   }
 
-  writingDiary = async (userId, diaryBookId, content) => {
-    const CONTENT_MAX_LEN = 240;
-
-    if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
-      throw BAD_REQUEST;
-    }
-    if (typeof content !== 'string' || !content || content.length > CONTENT_MAX_LEN) {
-      throw BAD_REQUEST;
-    }
-
-    const res = await axios.post('/repositories/diary', {
-      userId,
-      diaryBookId,
-      content
-    });
-
-    if (res.status === 400) {
-      throw BAD_REQUEST;
-    }
-  }
 }
+
+// class DiaryService {
+//   this.diaryModel =
+
+//   static getDiary = async (diaryBookId, page) => {
+//     if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
+//       throw BAD_REQUEST;
+//     }
+//     if (typeof page !== 'number' || page < 1) {
+//       throw BAD_REQUEST;
+//     }
+
+//     const res = await axios.get(`/repositories/diary-book/${diaryBookId}?page=${page}`);
+
+//     if (res.status === 400) {
+//       throw BAD_REQUEST;
+//     } else if (res.status === 404) {
+//       throw NOT_FOUND;
+//     }
+
+//     return {
+//       page: res.page,
+//       content: res.content
+//     };
+//   }
+
+//   writingDiary = async (userId, diaryBookId, content) => {
+//     const CONTENT_MAX_LEN = 240;
+
+//     if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
+//       throw BAD_REQUEST;
+//     }
+//     if (typeof content !== 'string' || !content || content.length > CONTENT_MAX_LEN) {
+//       throw BAD_REQUEST;
+//     }
+
+//     const res = await axios.post('/repositories/diary', {
+//       userId,
+//       diaryBookId,
+//       content
+//     });
+
+//     if (res.status === 400) {
+//       throw BAD_REQUEST;
+//     }
+//   }
+// }
 
 module.exports = DiaryService;
