8e1180de3838e9de49e3009c89ba5f5f7d60abdb
MinJunSeo <smj0385@naver.com>
2020-11-18 16:47:31 +0900

    feat: check string value is alright form

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const isUserDiaryBook = require('./isUserDiaryBook');
 const isIntegerArg = require('./isIntegerArg');
+const isStringArg = require('./isStringArg');
 
 module.exports = {
   isUserDiaryBook,
-  isIntegerArg
+  isIntegerArg,
+  isStringArg
 };
