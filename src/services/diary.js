6d3b8e4b003ff6ffecde5c54ded24739a9e5c305
MinJunSeo <smj0385@naver.com>
2020-11-18 16:57:53 +0900

    fix: service code change to constructor function

--- src/services/diary.js
+++ src/services/diary.js
@@ -1,12 +1,10 @@
 const axios = require('axios').default;
-const { Diary } = require('../repositories');
-const { isIntegerArg } = require('../utils');
+const { AUTHOR_LEN } = require('../configs/attribute');
+const { isIntegerArg, isStringArg } = require('../utils');
 const { BAD_REQUEST, NOT_FOUND_DIARY_BOOK, NOT_FOUND_PAGE } = require('../errors');
 
 function DiaryService() {
-  this.model = Diary;
-  
-  this.getDiary = async (diaryBookId, page) => {
+  this.getDiary = async function (diaryBookId, page) {
     if (!isIntegerArg(diaryBookId) || !isIntegerArg(page)) {
       throw BAD_REQUEST;
     }
@@ -22,56 +20,28 @@ function DiaryService() {
         throw NOT_FOUND_PAGE;
       }
     }
-    return res;
+    
+    return {
+      page: res.page,
+      content: res.content
+    };
   }
 
+  this.writingDiary = async function (userId, diaryBookId, content) {
+    if (!isIntegerArg(diaryBookId) || !isStringArg(content) || content.length > AUTHOR_LEN) {
+      throw BAD_REQUEST;
+    }
+
+    const res = await axios.post(`/repositories/diary`, {
+      userId,
+      diaryBookId,
+      content
+    });
+
+    if (res.status === 400) {
+      throw BAD_REQUEST;
+    }
+  }
 }
-
-// class DiaryService {
-//   this.diaryModel =
-
-//   static getDiary = async (diaryBookId, page) => {
-//     if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
-//       throw BAD_REQUEST;
-//     }
-//     if (typeof page !== 'number' || page < 1) {
-//       throw BAD_REQUEST;
-//     }
-
-//     const res = await axios.get(`/repositories/diary-book/${diaryBookId}?page=${page}`);
-
-//     if (res.status === 400) {
-//       throw BAD_REQUEST;
-//     } else if (res.status === 404) {
-//       throw NOT_FOUND;
-//     }
-
-//     return {
-//       page: res.page,
-//       content: res.content
-//     };
-//   }
-
-//   writingDiary = async (userId, diaryBookId, content) => {
-//     const CONTENT_MAX_LEN = 240;
-
-//     if (typeof diaryBookId !== 'number' || diaryBookId < 1) {
-//       throw BAD_REQUEST;
-//     }
-//     if (typeof content !== 'string' || !content || content.length > CONTENT_MAX_LEN) {
-//       throw BAD_REQUEST;
-//     }
-
-//     const res = await axios.post('/repositories/diary', {
-//       userId,
-//       diaryBookId,
-//       content
-//     });
-
-//     if (res.status === 400) {
-//       throw BAD_REQUEST;
-//     }
-//   }
-// }
 
 module.exports = DiaryService;
