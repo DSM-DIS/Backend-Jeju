0830237dd986364de61bc5656d341dc7d5b84985
MinJunSeo <smj0385@naver.com>
2020-11-20 15:45:56 +0900

    fix: isCreatedDiaryBook bad request error handler

--- src/utils/isCreatedDiaryBook.js
+++ src/utils/isCreatedDiaryBook.js
@@ -1,9 +1,12 @@
 const axios = require('axios').default;
+const { BAD_REQUEST } = require('../errors');
 
 const isCreatedDiaryBook = async (diaryBookId) => {
   const res = await axios.get(`/repositories/diay-books/${diaryBookId}`);
 
-  if (res.status === 404) {
+  if (res.status === 400) {
+    throw BAD_REQUEST;
+  } else if (res.status === 404) {
     return false;
   }
   return true;
