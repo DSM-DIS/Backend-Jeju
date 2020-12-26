316b567ec98dd84d35026b955fa228e01fb8f240
MinJunSeo <smj0385@naver.com>
2020-12-26 18:03:21 +0900

    fix: Cannot find module '../routes' error

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('../routes');
+const router = require('../api/routes');
 const { NotFoundApi } = require('../errors');
 
 const loadExpressApp = (app) => {
