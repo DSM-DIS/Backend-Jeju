1b384151789b3d739f39f692902577c9954ee1f6
MinJunSeo <smj0385@naver.com>
2020-12-26 16:44:26 +0900

    feat: diary router

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express')();
-const controller = require('./controller');
+const controller = require('../controller/diary');
 
 router.get('/:page', controller.getDiary);
 router.post('/', controller.writingDiary);
