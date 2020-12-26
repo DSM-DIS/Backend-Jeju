952f2b704706f89fa17a96abf204273ac497153f
MinJunSeo <smj0385@naver.com>
2020-12-26 16:08:10 +0900

    style: description some utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 const checkIntegerArg = require('./checkParam/checkIntegerArg');
 const checkStringArg = require('./checkParam/checkStringArg');
-const checkDiaryBook = require('./checkParam/checkDiaryBook');
-const checkOwner = require('./checkParam/checkOwner');
-const checkUser = require('./checkParam/checkUser');
+const checkDiaryBook = require('./checkParam/checkDiaryBook');  // 해당 diaryBookId가 만들어졌는지 확인한다.
+const checkOwner = require('./checkParam/checkOwner');          // 자신이 해당 diaryBook의 owner인지 확인한다.
+const checkUser = require('./checkParam/checkUser');            // 자신이 해당 diaryBook에 소속되어 있는지 확인한다.
 const httpErrorHandler = require('./httpErrorHandler');
 
 module.exports = {
