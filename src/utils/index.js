f06cb92e0d87e928d7eee0363eac667600aa4ed2
MinJunSeo <smj0385@naver.com>
2020-12-26 16:24:08 +0900

    fix: util name change

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ const checkIntegerArg = require('./checkParam/checkIntegerArg');
 const checkStringArg = require('./checkParam/checkStringArg');
 const checkDiaryBook = require('./checkParam/checkDiaryBook');  // 해당 diaryBookId가 만들어졌는지 확인한다.
 const checkOwner = require('./checkParam/checkOwner');          // 자신이 해당 diaryBook의 owner인지 확인한다.
-const checkUser = require('./checkParam/checkUser');            // 자신이 해당 diaryBook에 소속되어 있는지 확인한다.
+const checkYourDiary = require('./checkParam/checkYourDiary');            // 자신이 해당 diaryBook에 소속되어 있는지 확인한다.
 const httpErrorHandler = require('./httpErrorHandler');
 
 module.exports = {
@@ -10,6 +10,6 @@ module.exports = {
   checkStringArg,
   checkDiaryBook,
   checkOwner,
-  checkUser,
+  checkYourDiary,
   httpErrorHandler
 };
