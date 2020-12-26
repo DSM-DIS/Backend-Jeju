d8279e2cab9ebfb5b081fa3d33da23819f86d81d
MinJunSeo <smj0385@naver.com>
2020-12-26 14:59:01 +0900

    fix: error varialbe is undefined

--- src/utils/isCreatedDiaryBook.js
+++ src/utils/isCreatedDiaryBook.js
@@ -1,11 +1,11 @@
 const axios = require('axios').default;
-const { BAD_REQUEST } = require('../errors');
+const { BadRequest } = require('../errors');
 
 const isCreatedDiaryBook = async (diaryBookId) => {
   const res = await axios.get(`/repositories/diay-books/${diaryBookId}`);
 
   if (res.status === 400) {
-    throw BAD_REQUEST;
+    throw BadRequest;
   } else if (res.status === 404) {
     return false;
   }
--- src/utils/isOwner.js
+++ src/utils/isOwner.js
@@ -1,13 +1,13 @@
 const axios = require('axios').default;
-const { BAD_REQUEST, NOT_FOUND_DIARY_BOOK } = require('../errors');
+const { BadRequest, NotFoundDiaryBook} = require('../errors');
 
 const isOwner = async (userId, diaryBookId) => {
   const res = await axios.get(`/repositories/diay-books/${diaryBookId}`);
 
   if (res.status === 400) {
-    throw BAD_REQUEST;
+    throw BadRequest;
   } else if (res.status === 404) {
-    throw NOT_FOUND_DIARY_BOOK;
+    throw NotFoundDiaryBook;
   }
 
   return userId === res.owner;
