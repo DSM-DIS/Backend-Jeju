1a578a7364c25b2a1d71d039f5100bc24d47dbbf
MinJunSeo <smj0385@naver.com>
2020-12-29 12:02:20 +0900

    fix: util index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,7 @@
-const checkIntegerArg = require('./checkParam/checkIntegerArg');
-const checkStringArg = require('./checkParam/checkStringArg');
-const checkDiaryBook = require('./checkParam/checkDiaryBook');  // 해당 diaryBookId가 만들어졌는지 확인한다.
-const checkOwner = require('./checkParam/checkOwner');          // 자신이 해당 diaryBook의 owner인지 확인한다.
-const checkYourDiary = require('./checkParam/checkYourDiary');  // 자신이 해당 diaryBook에 소속되어 있는지 확인한다.
-const checkContent = require('./checkParam/checkContent');
-const httpErrorHandler = require('./httpErrorHandler');
+const { checkDiaryBook, checkOwner, checkContent } = require('./checkParam');
 
 module.exports = {
-  checkIntegerArg,
-  checkStringArg,
   checkDiaryBook,
   checkOwner,
-  checkYourDiary,
-  checkContent,
-  httpErrorHandler
+  checkContent
 };
