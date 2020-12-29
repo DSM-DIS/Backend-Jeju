8cd8507ed94542ee867185a0f40646a2a74ae277
MinJunSeo <smj0385@naver.com>
2020-12-29 15:05:55 +0900

    feat: axios setting

--- src/utils/checkParam/checkOwner.js
+++ src/utils/checkParam/checkOwner.js
@@ -1,12 +1,18 @@
-const axios = require('axios').default;
+const Axios = require('axios').default;
+const { baseURL } = require('../../configs');
 const { ForbiddenDiaryBook } = require('../../errors');
 const checkDiaryBook = require('./checkDiaryBook');
+
+const axios = Axios.create({
+  baseURL: baseURL.jeonju,
+  timeout: 1000
+});
 
 const checkOwner = async (userId, diaryBookId) => {
   // diaryBookId error handler
   await checkDiaryBook(diaryBookId);
 
-  const res = await axios.get(`/repositories/diay-books/${diaryBookId}`);
+  const res = await axios.get(`/diay-books/${diaryBookId}`);
   if (res.owner !== res.userId) {
     throw ForbiddenDiaryBook;
   }
