1831552e45785e0e1391f53069f88819942b3e89
MinJunSeo <smj0385@naver.com>
2020-12-29 11:59:05 +0900

    pref: checkParam dir use index.js

---
+++ src/utils/checkParam/index.js
@@ -0,0 +1,9 @@
+const checkDiaryBook = require('./checkDiaryBook');
+const checkOwner = require('./checkOwner');
+const checkContent = require('./checkContent');
+
+module.exports = {
+  checkDiaryBook,
+  checkOwner,
+  checkContent
+};
