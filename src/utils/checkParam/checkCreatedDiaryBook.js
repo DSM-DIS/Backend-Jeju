a9d5897e35992a300ddab02a44072995d6a882bd
MinJunSeo <smj0385@naver.com>
2020-12-29 11:41:24 +0900

    fix: error handler

---
+++ src/utils/checkParam/checkCreatedDiaryBook.js
@@ -0,0 +1,11 @@
+const axios = require('axios').default;
+const { NotFoundDiaryBook } = require('../../errors');
+
+const checkCreatedDiaryBook = async (diaryBookId) => {
+  const res = await axios.get(`repositories/diary-books/${diaryBookId}`);
+  if (res.status === 404) {
+    throw NotFoundDiaryBook;
+  }
+};
+
+module.exports = checkCreatedDiaryBook;
