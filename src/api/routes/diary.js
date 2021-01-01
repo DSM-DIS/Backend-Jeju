b7b2fe452105ea26553a4a62cc21662c37c83adc
MinJunSeo <smj0385@naver.com>
2021-01-01 11:53:02 +0900

    feat: logRequest middleware

--- src/api/routes/diary.js
+++ src/api/routes/diary.js
@@ -1,7 +1,8 @@
 const router = require('express')();
 const controller = require('../controller/diary');
+const logRequest = require('../../middlewares/log');
 
-router.get('/:id/diary/:page', controller.getDiary);
-router.post('/:id/diary', controller.writingDiary);
+router.get('/:id/diary/:page', logRequest, controller.getDiary);
+router.post('/:id/diary', logRequest, controller.writingDiary);
 
 module.exports = router;
